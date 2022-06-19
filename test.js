1bcd14cbe18282421e772f9688b88e13bec5f637
AntonBud <budchenkoai@gmail.com>
2022-06-19 16:23:23 +0300

    JS Strings slice &tags. Arrays basic

--- test.js
+++ test.js
@@ -13,3 +13,17 @@ var string1="abc";
 var string2="ABC";
 
 console.log(string1.toLowerCase === string2.toLowerCase);
+
+var str="hallo world";
+var str2=str.slice(2,9)
+console.log(str,"Sliced version is ", str2)
+
+var tags="meat, ham, chicken, pork, beef"
+console.log(tags.split(","))
+
+var myArray=[];
+myArray[0]=25
+myArray[1]=35
+myArray[3]=45
+
+console.log(myArray)
