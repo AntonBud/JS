b730c8092a6dddcaa28bf3628284be8537f9050b
AntonBud <budchenkoai@gmail.com>
2022-06-19 11:35:14 +0300

    JS if else

---
+++ test.js
@@ -0,0 +1,14 @@
+var Youlikemeat=false;
+var Mynum = 1;
+if (Mynum == 10){
+    document.write("Num is equal to 10")
+
+} else {
+    document.write("My num is not equal to 10")
+}
+
+if (Youlikemeat){
+    document.write("Meat menu")
+}else{
+    document.write("vegan menu")
+}
