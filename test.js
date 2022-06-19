286fdd4d70371596b898b10757ff2dfa61f47dbf
AntonBud <budchenkoai@gmail.com>
2022-06-19 15:50:36 +0300

    JS strings

--- test.js
+++ test.js
@@ -1,20 +1,15 @@
-var a=5;
-var b=5;
-console.log(a+b);
-console.log(typeof(a+b))
+var myString="I'm a string\"Ninja\""
+console.log(myString.length)
 
-console.log(Math.round(7.8))
-console.log(Math.max(7,2,4,5))
-console.log(Math.PI)
-
-var a=6;
-var b=5;
-console.log(a*b);
-console.log(typeof(a+b))
-
-if(isNaN(a)){
-   console.log("not a number");
-
-}else{
-    console.log("meaning offunchion is"+ (a+b));
+console.log(myString.toUpperCase())
+console.log(myString.indexOf("Ninja"))
+if (myString.indexOf("Ninja")=== -1){
+    console.log("Ninja is not in the string")
+} else{
+    console.log("Ninja is here, on "+ myString.indexOf("Ninja")+" place")
 }
+
+var string1="abc";
+var string2="ABC";
+
+console.log(string1.toLowerCase === string2.toLowerCase);
