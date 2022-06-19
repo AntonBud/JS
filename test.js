39983d5c2ee53db087a057ef8ebc1820d2e1f056
AntonBud <budchenkoai@gmail.com>
2022-06-19 17:38:44 +0300

    JS "This" keyword in objects

--- test.js
+++ test.js
@@ -1,25 +1,19 @@
-var myArray=new Array()
-myArray[0]=8
-myArray[1]="Hallo"
 
-var myCar=new Object();
-myCar.maxSpeed=200;
-myCar.Driver="Vasya"
-console.log(myCar.Driver)
-
-
-
-myCar.Drive= function(){
-    console.log("Now driving");
-};
-
-
-myCar.Drive();
 var myCar0={
     maxSpeed:210,
-     Driver:"Petya",
-      drive:function(speed, time){console.log(speed*time);}
+    Driver:"Petya",
+    drive:function(speed, time){
+        console.log(speed*time);},
+    logDriver:function(){
+        console.log("Driver name is "+ this.Driver);
+    }
     }
 
+    myCar0.logDriver()
+
+
+    
     console.log(myCar0.maxSpeed);
     myCar0.drive(120, 3)
+
+    console.log(this)
